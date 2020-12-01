d7ecb57af08cd32f2c2a61a919c97f6221382066
LinHuiqing <lin.hui.qing1999@gmail.com>
2020-12-01 13:13:33 +0800

    part 2 with evval script implemented

--- preprocess.py
+++ preprocess.py
@@ -1,3 +1,5 @@
+from model import hmm
+
 def read_paired_file(filename):
     with open(filename, encoding = 'utf-8') as f:
         file_content = f.read()
@@ -14,3 +16,24 @@ def read_paired_file(filename):
     return l2
 
 # TODO: Implement reader for test data
+
+def read_observation_file(filename):
+    with open(filename) as f:
+        file_content = f.read()
+    # sort file into comments, with each comment being an element of the list
+    l = file_content.strip().split('\n\n')
+    
+    # each word-line is then made into an element within each comment element
+    l2 = [i.split('\n') for i in l]
+
+    return l2
+
+if __name__ == "__main__":
+    datasets = ["data/SG", "data/EN", "data/CN"]
+    for ds in datasets:
+        train_data = read_paired_file("{}/train".format(ds))
+        model = hmm.HMM()
+        model.train(train_data)
+        test_data = read_observation_file("{}/dev.in".format(ds))
+        model.predict_part2(test_data) \
+             .write_preds("{}/part2_dev.prediction".format(ds))
---
+++ run_eval.sh
@@ -0,0 +1,3 @@
+python EvalScript/evalResult.py data/EN/dev.out data/EN/$1_dev.prediction
+python EvalScript/evalResult.py data/CN/dev.out data/CN/$1_dev.prediction
+python EvalScript/evalResult.py data/SG/dev.out data/SG/$1_dev.prediction
